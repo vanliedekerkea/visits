0b96a1fd10589caeefa7b59e06f94f0c9e429e9e
Arno Van Liedekerke <arnovanliedekerke@ptr-ek1h2nj5d7so0iurgno.18120a2.ip6.access.telenet.be>
2022-08-22 22:19:24 +0200

    Error handling in case visit is not found

--- src/main/java/com/office/visits/controllers/VisitController.java
+++ src/main/java/com/office/visits/controllers/VisitController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.office.visits.model.Visit;
 import com.office.visits.services.VisitService;
@@ -33,7 +34,8 @@ public class VisitController {
 			return new ResponseEntity<>(visit, HttpStatus.CREATED);
 		}
 		else {
-			return null;
+			throw new ResponseStatusException(
+					HttpStatus.INTERNAL_SERVER_ERROR, "Visit not saved properly");
 		}
 	}
 

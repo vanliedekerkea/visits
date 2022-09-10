36fae69884495b6981218798900bada369e804d3
Arno Van Liedekerke <arnovanliedekerke@ptr-ek1h2nj5d7so0iurgno.18120a2.ip6.access.telenet.be>
2022-09-10 19:24:14 +0200

    feat(19): Create product controller

---
+++ src/main/java/com/office/visits/controllers/ProductController.java
@@ -0,0 +1,66 @@
+package com.office.visits.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.office.visits.model.Product;
+import com.office.visits.services.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+	@Autowired
+	ProductService productService;
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Product>> findAll() {
+		return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Product> createProduct(@RequestBody Product newProduct) {
+		Product product = productService.save(newProduct);
+		if (product != null) {
+			return new ResponseEntity<>(product, HttpStatus.CREATED);
+		} else {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Product not saved properly");
+		}
+	}
+
+	@GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Product> getProduct(@PathVariable("id") Long id) {
+		Optional<Product> optionalProduct = productService.getProduct(id);
+		if (optionalProduct.isPresent()) {
+			return new ResponseEntity<>(optionalProduct.get(), HttpStatus.OK);
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+		}
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<Product> deleteProduct(@PathVariable("id") Long id) {
+		productService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PutMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product productToUpdate) {
+		return new ResponseEntity<>(productService.updateProduct(id, productToUpdate), HttpStatus.OK);
+	}
+
+}

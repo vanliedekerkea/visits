0e6662d019e2c0e45436edf02a3bf5d2a2bd4e93
Arno Van Liedekerke <arnovanliedekerke@ptr-ek1h2nj5d7so0iurgno.18120a2.ip6.access.telenet.be>
2022-09-26 18:44:58 +0200

    fix(45): Unit test for checking that spring starts up correctly created

--- src/test/java/com/office/visits/VisitsApplicationTests.java
+++ src/test/java/com/office/visits/VisitsApplicationTests.java
@@ -3,6 +3,7 @@ package com.office.visits;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//@ExtendWith(SpringExtension.class) --> use this for unittests
 @SpringBootTest
 class VisitsApplicationTests {
 

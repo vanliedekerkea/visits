a2abfea0a666f67ffac9cc15848c8b361cc5c60b
Arno Van Liedekerke <arnovanliedekerke@ptr-ek1h2nj5d7so0iurgno.18120a2.ip6.access.telenet.be>
2022-10-02 22:06:37 +0200

    feat(53): Model for bill and update mappers

--- src/test/java/com/office/visits/dto/priceItem/mapper/PriceItemDTOToPriceItemTest.java
+++ src/test/java/com/office/visits/dto/priceItem/mapper/PriceItemDTOToPriceItemTest.java
@@ -16,9 +16,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.office.visits.dto.priceItem.PriceItemDTO;
+import com.office.visits.model.Bill;
 import com.office.visits.model.Price;
 import com.office.visits.model.PriceItem;
 import com.office.visits.model.Visit;
+import com.office.visits.repositories.BillRepository;
 import com.office.visits.repositories.PriceRepository;
 import com.office.visits.repositories.VisitRepository;
 
@@ -34,6 +36,9 @@ class PriceItemDTOToPriceItemTest {
 	@Mock
 	PriceRepository priceRepository;
 
+	@Mock
+	BillRepository billRepository;
+	
 	@BeforeEach
 	public void initMocks() {
 		MockitoAnnotations.openMocks(this);
@@ -50,19 +55,24 @@ class PriceItemDTOToPriceItemTest {
 		final long PRICE_ID = 10;
 		final long VISIT_ID = 20;
 		final long PRICE_ITEM_ID = 30;
+		final long BILL_ID = 40;
 		final BigDecimal FINAL_PRICE = new BigDecimal(19.90);
 		final BigDecimal QUANTITY = new BigDecimal(1);
 		priceItemDTO.setPriceId(PRICE_ID);
 		priceItemDTO.setVisitId(VISIT_ID);
-		priceItemDTO.setPriceItemId(PRICE_ITEM_ID);
+		priceItemDTO.setId(PRICE_ITEM_ID);
+		priceItemDTO.setBillId(BILL_ID);
 		priceItemDTO.setFinalPrice(FINAL_PRICE);
 		priceItemDTO.setQuantity(QUANTITY);
 		Visit visitMock = new Visit();
 		visitMock.setId(VISIT_ID);
 		Price priceMock = new Price();
 		priceMock.setId(PRICE_ID);
+		Bill billMock = new Bill();
+		billMock.setId(BILL_ID);
 		Mockito.when(visitRepository.getReferenceById(VISIT_ID)).thenReturn(visitMock);
 		Mockito.when(priceRepository.getReferenceById(PRICE_ID)).thenReturn(priceMock);
+		Mockito.when(billRepository.getReferenceById(BILL_ID)).thenReturn(billMock);
 		PriceItem priceItem = Optional.of(priceItemDTO).map(priceItemDTOToPriceItem).get();
 		assertNotNull(priceItem);
 		assertEquals(PRICE_ITEM_ID, priceItem.getId());
@@ -70,6 +80,7 @@ class PriceItemDTOToPriceItemTest {
 		assertEquals(QUANTITY, priceItem.getQuantity());
 		assertEquals(PRICE_ID, priceItem.getPrice().getId());
 		assertEquals(VISIT_ID, priceItem.getVisit().getId());
+		assertEquals(BILL_ID, priceItem.getBill().getId());
 	}
 
 }
--- src/test/java/com/office/visits/dto/priceItem/mapper/PriceItemToPriceItemDTOTest.java
+++ src/test/java/com/office/visits/dto/priceItem/mapper/PriceItemToPriceItemDTOTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.Assert;
 
 import com.office.visits.dto.priceItem.PriceItemDTO;
+import com.office.visits.model.Bill;
 import com.office.visits.model.Price;
 import com.office.visits.model.PriceItem;
 import com.office.visits.model.Visit;
@@ -19,19 +20,23 @@ import com.office.visits.model.Visit;
 class PriceItemToPriceItemDTOTest {
 
 	@Test
-	void testAppXly() {
+	void testApply() {
 		final long VISIT_ID = 20;
 		final long PRICE_ID = 10;
 		final long PRICE_ITEM_ID = 30;
+		final long BILL_ID = 40;
 		final BigDecimal FINAL_PRICE = new BigDecimal(19.90);
 		final BigDecimal QUANTITY = new BigDecimal(1);
 		Visit visit = new Visit();
 		visit.setId(VISIT_ID);
 		Price price = new Price();
 		price.setId(PRICE_ID);
+		Bill bill = new Bill();
+		bill.setId(BILL_ID);
 		PriceItem priceItem = new PriceItem();
 		priceItem.setVisit(visit);
 		priceItem.setPrice(price);
+		priceItem.setBill(bill);
 		priceItem.setId(PRICE_ITEM_ID);
 		priceItem.setFinalPrice(FINAL_PRICE);
 		priceItem.setQuantity(QUANTITY);
@@ -39,6 +44,7 @@ class PriceItemToPriceItemDTOTest {
 		Assert.notNull(priceItemDTO, "PriceItemDTO is null");
 		assertEquals(VISIT_ID, priceItemDTO.getVisitId());
 		assertEquals(PRICE_ID, priceItemDTO.getPriceId());
+		assertEquals(BILL_ID, priceItemDTO.getBillId());
 		assertEquals(PRICE_ITEM_ID, priceItemDTO.getId());
 		assertEquals(FINAL_PRICE, priceItemDTO.getFinalPrice());
 		assertEquals(QUANTITY, priceItemDTO.getQuantity());
